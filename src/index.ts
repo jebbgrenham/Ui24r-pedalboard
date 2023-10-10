078c640eb2e01e7bc349a5df081f5a5b4a84b8ac
jebbgrenham <jebbgrenham@gmail.com>
2023-10-10 11:16:19 +0100

    Quick and dirty test to prove we can control the mixer with the library. Success!

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,52 @@ console.log('Welcome application');
 const conn = new SoundcraftUI("10.0.1.2");
 conn.connect();
 
-console.log(conn);
+//console.log(conn);
 
 //conn.disconnect(); // close connection
 //conn.reconnect(); // close connection and reconnect after timeout
+
+console.log('Ill print first');
+
+setTimeout(() => {
+    conn.muteGroup(1).toggle()
+    console.log('Mute 1 toggled');
+}, 2000);
+
+setTimeout(() => {
+    conn.muteGroup(1).toggle()
+    console.log('Mute 1 toggled');
+}, 4000);
+
+setTimeout(() => {
+    conn.muteGroup(1).toggle()
+    console.log('Mute 1 toggled');
+}, 6000);
+
+setTimeout(() => {
+    conn.muteGroup(1).toggle()
+    console.log('Mute 1 toggled');
+}, 8000);
+
+setTimeout(() => {
+    conn.muteGroup(1).toggle()
+    console.log('Mute 1 toggled');
+}, 10000);
+
+setTimeout(() => {
+    conn.muteGroup(1).toggle()
+    console.log('Mute 1 toggled');
+}, 12000);
+
+setTimeout(() => {
+    conn.muteGroup(1).toggle()
+    console.log('Mute 1 toggled');
+}, 14000);
+
+setTimeout(() => {
+    conn.muteGroup(1).toggle()
+    console.log('Mute 1 toggled');
+}, 16000);
+
 
 
