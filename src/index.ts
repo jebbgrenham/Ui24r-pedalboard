f9423275c7d9f56782dd955ee5bdf6666e6f14c2
jebbgrenham <jebbgrenham@gmail.com>
2023-10-11 00:40:18 +0100

    added LED logic for the 1st LED. Working

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,72 @@
-import { SoundcraftUI } from 'soundcraft-ui-connection';
-import { filter } from 'rxjs/operators';
-
-const conn = new SoundcraftUI("10.0.1.2");
-conn.connect();
+import { SoundcraftUI } from 'soundcraft-ui-connection'
+import { filter } from 'rxjs/operators'
+const conn = new SoundcraftUI("10.0.1.2")
+conn.connect()
 //console.log(conn);
 //conn.disconnect(); // close connection
 //conn.reconnect(); // close connection and reconnect after timeout
-
-console.log('Started');
+console.log('Started')
 
 function muteToggle(x:number) {
-  conn.muteGroup(x).toggle()  
-  conn.muteGroup(x).state$.subscribe((state) => {
-  });
+  conn.muteGroup(x).toggle()
 }
 
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(9, 'out'); //use GPIO pin 9 as output
-var pushButton = new Gpio(5, 'in', 'rising', {debounceTimeout: 75}); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
-var occurrence = 0;
-pushButton.watch(function (err:string, value:string) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
+var LED1 = new Gpio(9, 'out') //use GPIO pins as output
+var LED2 = new Gpio(10, 'out')
+var LED3 = new Gpio(11, 'out')
+var LED4 = new Gpio(12, 'out')
+var pushButton1 = new Gpio(5, 'in', 'rising', {debounceTimeout: 75}) //use GPIO pins as input
+var pushButton2 = new Gpio(6, 'in', 'rising', {debounceTimeout: 75})
+var pushButton3 = new Gpio(7, 'in', 'rising', {debounceTimeout: 75})
+var pushButton4 = new Gpio(8, 'in', 'rising', {debounceTimeout: 75})
+
+conn.muteGroup(1).state$.subscribe((state) => { //keep LED in correct state
+  console.log('Should LED to:', state)
+  LED1.writeSync(state)
+  console.log(LED1.readSync())
+});
+
+pushButton1.watch(function (err:string, value:string) { //Watch for hardware interrupts
   if (err) { //if an error
     console.error('There was an error', err); //output error message to console
   return;
   }
-  occurrence ++;
-  console.log('BUTTON Action',occurrence);
   muteToggle(1)
-  LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
+//  LED1.writeSync(value)
+//  conn.muteGroup(1).state$.subscribe((state) => {
+//  console.log('Mute 1 is:', state);
+//  });
+});
+pushButton2.watch(function (err:string, value:string) { //Watch for hardware interrupts
+  if (err) { //if an error
+    console.error('There was an error', err); //output error message to console
+  return;
+  }
+  muteToggle(2)
+});
+pushButton3.watch(function (err:string, value:string) { //Watch for hardware interrupts
+  if (err) { //if an error
+    console.error('There was an error', err); //output error message to console
+  return;
+  }
+  muteToggle(3)
+});
+pushButton4.watch(function (err:string, value:string) { //Watch for hardware interrupts
+  if (err) { //if an error
+    console.error('There was an error', err); //output error message to console
+  return;
+  }
+  muteToggle(4)
 });
 
+
+
+
 function unexportOnClose() { //function to run when exiting program
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport LED GPIO to free resources
-  pushButton.unexport(); // Unexport Button GPIO to free resources
+  LED1.writeSync(0); // Turn LED off
+  LED1.unexport(); // Unexport LED GPIO to free resources
+  pushButton1.unexport(); // Unexport Button GPIO to free resources
 };
 
 process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
