820d8125f22de8f6d15f3001d07e642015ea92ec
jebbgrenham <jebbgrenham@gmail.com>
2023-10-11 03:06:09 +0100

    code optimization

--- build/index.js
+++ build/index.js
@@ -11,12 +11,9 @@ function muteToggle(x) {
     conn.muteGroup(x).toggle();
 }
 function handleButtonEvent(buttonNumber) {
-    return function (err, value) {
-        if (err) {
-            console.error('There was an error', err);
-            return;
-        }
-        muteToggle(buttonNumber);
+    return (err, value) => {
+        if (!err)
+            muteToggle(buttonNumber);
     };
 }
 function subscribeAndControlLED(//make the LEDs read mutegroup states
@@ -26,23 +23,17 @@ muteGroupNumber, LED) {
         //    console.log(`LED${muteGroupNumber} set to:`, LED.readSync())
     });
 }
-var Gpio = require('onoff').Gpio; //set up all the variables for the GPIO pins
+const Gpio = require('onoff').Gpio;
 const ledPinNumbers = [9, 10, 11, 12];
 const pushButtonPins = [5, 6, 7, 8];
 const LED = ledPinNumbers.map((pin) => new Gpio(pin, 'out'));
 const pushButtons = pushButtonPins.map((pin) => new Gpio(pin, 'in', 'rising', { debounceTimeout: 75 }));
 const [LED1, LED2, LED3, LED4] = LED;
 const [pushButton1, pushButton2, pushButton3, pushButton4] = pushButtons;
-// Watch buttons using the handleButtonEvent function
-pushButton1.watch(handleButtonEvent(1));
-pushButton2.watch(handleButtonEvent(2));
-pushButton3.watch(handleButtonEvent(3));
-pushButton4.watch(handleButtonEvent(4));
-// update the LEDs constantly
-subscribeAndControlLED(1, LED1);
-subscribeAndControlLED(2, LED2);
-subscribeAndControlLED(3, LED3);
-subscribeAndControlLED(4, LED4);
+const buttons = [pushButton1, pushButton2, pushButton3, pushButton4];
+const leds = [LED1, LED2, LED3, LED4];
+buttons.forEach((button, index) => button.watch(handleButtonEvent(index + 1)));
+leds.forEach((led, index) => subscribeAndControlLED(index + 1, led));
 function unexportOnClose() {
     LED1.writeSync(0); // Turn LED off
     LED1.unexport(); // Unexport LED GPIO to free resources
--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,10 @@ function muteToggle(x:number) {
   conn.muteGroup(x).toggle()
 }
 
-function handleButtonEvent(buttonNumber: number) {// Handle button events
-  return function (err: string, value: string) {
-    if (err) {
-      console.error('There was an error', err);
-      return;
-    }
-    muteToggle(buttonNumber);
-  };
+function handleButtonEvent(buttonNumber: number) {
+  return (err: string, value: string) => {
+    if (!err) muteToggle(buttonNumber)
+  }
 }
 
 function subscribeAndControlLED( //make the LEDs read mutegroup states
@@ -31,27 +27,18 @@ function subscribeAndControlLED( //make the LEDs read mutegroup states
   });
 }
 
-
-var Gpio = require('onoff').Gpio; //set up all the variables for the GPIO pins
+const Gpio = require('onoff').Gpio;
 const ledPinNumbers = [9, 10, 11, 12];
 const pushButtonPins = [5, 6, 7, 8];
 const LED = ledPinNumbers.map((pin) => new Gpio(pin, 'out'));
-const pushButtons = pushButtonPins.map(
-  (pin) => new Gpio(pin, 'in', 'rising', { debounceTimeout: 75 })
-);
+const pushButtons = pushButtonPins.map((pin) => new Gpio(pin, 'in', 'rising', { debounceTimeout: 75 }));
 const [LED1, LED2, LED3, LED4] = LED;
 const [pushButton1, pushButton2, pushButton3, pushButton4] = pushButtons;
+const buttons = [pushButton1, pushButton2, pushButton3, pushButton4];
+const leds = [LED1, LED2, LED3, LED4];
 
-// Watch buttons using the handleButtonEvent function
-pushButton1.watch(handleButtonEvent(1));
-pushButton2.watch(handleButtonEvent(2));
-pushButton3.watch(handleButtonEvent(3));
-pushButton4.watch(handleButtonEvent(4));
-// update the LEDs constantly
-subscribeAndControlLED(1, LED1);
-subscribeAndControlLED(2, LED2);
-subscribeAndControlLED(3, LED3);
-subscribeAndControlLED(4, LED4);
+buttons.forEach((button, index) => button.watch(handleButtonEvent(index + 1)));
+leds.forEach((led, index) => subscribeAndControlLED(index + 1, led));
 
 function unexportOnClose() { //function to run when exiting program
   LED1.writeSync(0); // Turn LED off
