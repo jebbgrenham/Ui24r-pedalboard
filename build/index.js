397244f98c47c7669e443262a04de724c6ac95bc
jebbgrenham <jebbgrenham@gmail.com>
2023-10-11 09:54:31 +0100

    Iterate through 3 pages of 4 footswitches each, some yet to be assigned but all pages work. Iteration switch is GPIO4

--- build/index.js
+++ build/index.js
@@ -3,17 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const soundcraft_ui_connection_1 = require("soundcraft-ui-connection");
 const conn = new soundcraft_ui_connection_1.SoundcraftUI("10.0.1.2");
 conn.connect();
-//console.log(conn);
-//conn.disconnect(); // close connection
-//conn.reconnect(); // close connection and reconnect after timeout
 console.log('Started');
-function muteToggle(x) {
-    conn.muteGroup(x).toggle();
+var mode = "mutesA";
+function stopButtonListeners() {
+    buttons.forEach((button) => button.unwatchAll());
 }
 function handleButtonEvent(buttonNumber) {
     return (err, value) => {
         if (!err)
-            muteToggle(buttonNumber);
+            conn.muteGroup(buttonNumber).toggle();
+        console.log('buttonhandle', buttonNumber);
+    };
+}
+function handleSamplerEvent(buttonNumber) {
+    return (err, value) => {
+        if (!err)
+            console.log('sample play'); //conn.muteGroup(buttonNumber).toggle()
     };
 }
 function subscribeAndControlLED(//make the LEDs read mutegroup states
@@ -25,19 +30,49 @@ muteGroupNumber, LED) {
 }
 const Gpio = require('onoff').Gpio;
 const ledPinNumbers = [9, 10, 11, 12];
-const pushButtonPins = [5, 6, 7, 8];
+const pushButtonPins = [5, 6, 7, 8,];
+const modeButton = new Gpio(4, 'in', 'rising', { debounceTimeout: 75 });
 const LED = ledPinNumbers.map((pin) => new Gpio(pin, 'out'));
 const pushButtons = pushButtonPins.map((pin) => new Gpio(pin, 'in', 'rising', { debounceTimeout: 75 }));
 const [LED1, LED2, LED3, LED4] = LED;
 const [pushButton1, pushButton2, pushButton3, pushButton4] = pushButtons;
 const buttons = [pushButton1, pushButton2, pushButton3, pushButton4];
 const leds = [LED1, LED2, LED3, LED4];
-buttons.forEach((button, index) => button.watch(handleButtonEvent(index + 1)));
-leds.forEach((led, index) => subscribeAndControlLED(index + 1, led));
+modeButton.watch((err, value) => {
+    if (err) {
+        throw err;
+    }
+    if (value === 1) {
+        stopButtonListeners();
+        if (mode === "mutesA") {
+            mode = "mutesB";
+        }
+        else if (mode === "mutesB") {
+            mode = "sampler";
+        }
+        else if (mode === "sampler") {
+            mode = "mutesA";
+        }
+        console.log(mode, '\n');
+        if (mode === "mutesA") {
+            buttons.forEach((button, index) => button.watch(handleButtonEvent(index + 1)));
+            leds.forEach((led, index) => subscribeAndControlLED(index + 1, led));
+        }
+        else if (mode === "mutesB") {
+            buttons.forEach((button, index) => button.watch(handleButtonEvent(index + 5)));
+        }
+        else if (mode === "sampler") {
+            buttons.forEach((button, index) => button.watch(handleSamplerEvent(index + 1)));
+        }
+    }
+});
 function unexportOnClose() {
-    LED1.writeSync(0); // Turn LED off
-    LED1.unexport(); // Unexport LED GPIO to free resources
-    pushButton1.unexport(); // Unexport Button GPIO to free resources
+    leds.forEach((led) => {
+        led.writeSync(0);
+        led.unexport();
+    });
+    pushButtons.forEach((button) => {
+        button.unexport();
+    });
 }
-;
 process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,23 @@ import { SoundcraftUI } from 'soundcraft-ui-connection'
 import { filter } from 'rxjs/operators'
 const conn = new SoundcraftUI("10.0.1.2")
 conn.connect()
-//console.log(conn);
-//conn.disconnect(); // close connection
-//conn.reconnect(); // close connection and reconnect after timeout
 console.log('Started')
+var mode = "mutesA"
 
-function muteToggle(x:number) {
-  conn.muteGroup(x).toggle()
+function stopButtonListeners() {
+  buttons.forEach((button) => button.unwatchAll());
 }
 
 function handleButtonEvent(buttonNumber: number) {
   return (err: string, value: string) => {
-    if (!err) muteToggle(buttonNumber)
+    if (!err) conn.muteGroup(buttonNumber).toggle()
+    console.log('buttonhandle', buttonNumber)
+  }
+}
+
+function handleSamplerEvent(buttonNumber: number) {
+  return (err: string, value: string) => {
+    if (!err) console.log('sample play') //conn.muteGroup(buttonNumber).toggle()
   }
 }
 
@@ -29,7 +34,8 @@ function subscribeAndControlLED( //make the LEDs read mutegroup states
 
 const Gpio = require('onoff').Gpio;
 const ledPinNumbers = [9, 10, 11, 12];
-const pushButtonPins = [5, 6, 7, 8];
+const pushButtonPins = [5, 6, 7, 8, ];
+const modeButton = new Gpio(4, 'in', 'rising', { debounceTimeout: 75 })
 const LED = ledPinNumbers.map((pin) => new Gpio(pin, 'out'));
 const pushButtons = pushButtonPins.map((pin) => new Gpio(pin, 'in', 'rising', { debounceTimeout: 75 }));
 const [LED1, LED2, LED3, LED4] = LED;
@@ -37,13 +43,35 @@ const [pushButton1, pushButton2, pushButton3, pushButton4] = pushButtons;
 const buttons = [pushButton1, pushButton2, pushButton3, pushButton4];
 const leds = [LED1, LED2, LED3, LED4];
 
-buttons.forEach((button, index) => button.watch(handleButtonEvent(index + 1)));
-leds.forEach((led, index) => subscribeAndControlLED(index + 1, led));
+modeButton.watch((err: any, value: any) => {
+  if (err) {
+    throw err;
+  }
+  if (value === 1) {
+  stopButtonListeners()
+  if (mode === "mutesA"){ mode = "mutesB" } else if (mode === "mutesB"){ mode = "sampler" } else if (mode === "sampler"){mode = "mutesA" }
+  console.log(mode, '\n')
+  if (mode === "mutesA") {
+  buttons.forEach((button, index) => button.watch(handleButtonEvent(index + 1)));
+  leds.forEach((led, index) => subscribeAndControlLED(index + 1, led));
+} else if (mode === "mutesB") {
+  buttons.forEach((button, index) => button.watch(handleButtonEvent(index + 5)));
+} else if (mode === "sampler") {
+  buttons.forEach((button, index) => button.watch(handleSamplerEvent(index + 1)));
+}
 
-function unexportOnClose() { //function to run when exiting program
-  LED1.writeSync(0); // Turn LED off
-  LED1.unexport(); // Unexport LED GPIO to free resources
-  pushButton1.unexport(); // Unexport Button GPIO to free resources
-};
+}
+});
+
+function unexportOnClose() {
+  leds.forEach((led) => {
+    led.writeSync(0);
+    led.unexport();
+  });
+
+  pushButtons.forEach((button) => {
+    button.unexport();
+  });
+}
 
 process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
