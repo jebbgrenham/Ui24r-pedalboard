3c8354a45b3c8a89aa41c3bd7980a31619ac6ade
jebbgrenham <jebbgrenham@gmail.com>
2023-10-14 04:12:11 +0100

    refactored entire code

--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,152 @@
-import { SoundcraftUI } from 'soundcraft-ui-connection'
-import { filter } from 'rxjs/operators'
-var player = require('play-sound')() 
-const conn = new SoundcraftUI("10.0.1.2")
-conn.connect()
-console.log('Started Program')
-console.log('YOU CHANGED IP YOU FOOL')
-let mode = "mutesA"
-console.log('Started in mode: ', mode)
+import { SoundcraftUI } from 'soundcraft-ui-connection';
+import { interval } from 'rxjs';
+const player = require('play-sound')();
+const Gpio = require('onoff').Gpio;
 
-function subscribeLED(LEDindex: number,LED: { writeSync: (state: number) => void, readSync: () => number } ) {
+// Initialize the Soundcraft UI connection
+const conn = new SoundcraftUI("10.0.1.2");
+conn.connect();
+
+// Define your modes and initial mode
+const modes = ["mutesA", "mutesB", "sampler", "player"];
+let modeIndex = 0;
+
+// Define LED and button pins
+const ledPinNumbers = [9, 10, 11, 12];
+const pushButtonPins = [5, 6, 7, 8];
+const LED = ledPinNumbers.map((pin) => new Gpio(pin, 'out'));
+const pushButtons = pushButtonPins.map((pin) => new Gpio(pin, 'in', 'rising', { debounceTimeout: 75 }));
+
+// LED and button arrays
+const [LED1, LED2, LED3, LED4] = LED;
+const [pushButton1, pushButton2, pushButton3, pushButton4] = pushButtons;
+const buttons = [pushButton1, pushButton2, pushButton3, pushButton4];
+const leds = [LED1, LED2, LED3, LED4];
+
+// Define the LED index mapping
+const ledIndexMap: { [mode: string]: (number | string)[] } = {
+  mutesA: [1, 2, 3, 4],
+  mutesB: [5, 6, 'fx', 'all'],
+};
+
+// Create a map to track subscriptions
+const subscriptionMap: { [index: number | string]: any } = {};
+
+// Function to subscribe LEDs
+function subscribeLED(LEDindex: number | string, LED: { writeSync: (state: number) => void, readSync: () => number }) {
   let index: number | string = LEDindex;
-  //if mode is mutes A.... but will this subscribe when mode changes? NO IT WON'T? Need console.
-  if (mode === 'mutesA'){ console.log('MODE UPDATED') }
-    conn.muteGroup(index as any).state$.subscribe((state) => {
-      LED.writeSync(state);
-    });
-
+  if (subscriptionMap[index]) {
+    subscriptionMap[index].unsubscribe(); // Unsubscribe previous subscription
+  }
+  subscriptionMap[index] = conn.muteGroup(index as any).state$.subscribe((state) => {
+    LED.writeSync(state);
+    console.log(`Read index: ${LEDindex} and set to:`, LED.readSync());
+  });
 }
 
-function stopButtonListeners() {
-  buttons.forEach((button) => button.unwatchAll());
+// Function to unsubscribe LEDs
+function unsubscribeLEDs() {
+  for (const index in subscriptionMap) {
+    if (subscriptionMap.hasOwnProperty(index)) {
+      subscriptionMap[index].unsubscribe();
+      delete subscriptionMap[index];
+    }
+  }
 }
 
 function handleMuteEvent(buttonNumber: number) {
   return (err: string, value: string) => {
     if (!err) {
-      if (buttonNumber == 7) { conn.muteGroup('fx').toggle() }
-      else if (buttonNumber == 8) { conn.muteGroup('all').toggle() } 
-      else {conn.muteGroup(buttonNumber).toggle() }
-      console.log('Pushed Button:', buttonNumber)
-      let stepUp = 1
-      if (mode == 'mutesB'){ stepUp = 5 }
-//      console.log(stepUp)
-      leds.forEach((led, index) => subscribeAndControlLED(index + stepUp, led));
+      const group = ledIndexMap[mode][buttonNumber - 1];
+      console.log(mode);
+      console.log(group);
+      if (typeof group === 'number') {
+        conn.muteGroup(group as any).toggle();
+      } else if (typeof group === 'string') {
+        conn.muteGroup(group as any).toggle();
+      }
+      console.log('Pushed Button:', group);
     }
-  }
+  };
 }
 
+
+// Function to handle sampler events
 function handleSamplerEvent(buttonNumber: number) {
   return (err: string, value: string) => {
-    if (!err) { 
-      player.play('./samples/' + buttonNumber + '.wav', (err: string) => {
-      if (err) console.log(`Could not play sound: ${err}`);
-      console.log('Played sample', buttonNumber)
+    if (!err) {
+      var audio = player.play('./samples/' + buttonNumber + '.wav', (err: string) => {
+        if (err) console.log(`Could not play sound: ${err}`);
+        pushButton1.watch(audio.kill());  
+        console.log('Played sample', buttonNumber);
       });
     }
-  }
+  };
 }
 
-function subscribeAndControlLED(
-  muteGroupNumber: number,
-  LED: { writeSync: (state: number) => void, readSync: () => number }
-): void {
-  let muteGroup: number | string = muteGroupNumber;
-  if (muteGroupNumber === 7) {
-    muteGroup = 'fx';
-  } else if (muteGroupNumber === 8) {
-    muteGroup = 'all';
-  }
-  console.log('mutegroupnumber is ', muteGroup);
-  conn.muteGroup(muteGroup as any).state$.subscribe((state) => {
-    LED.writeSync(state);
-    console.log(`LED${muteGroup} set to:`, LED.readSync());
-  });
+
+function handlePlayerEvent(buttonNumber: number) {
+  return (err: string, value: string) => {
+    if (!err) {
+/*      player.play('./samples/' + buttonNumber + '.wav', (err: string) => {
+        if (err) console.log(`Could not play sound: ${err}`);
+        console.log('Played sample', buttonNumber);
+      });
+*/      
+    }
+  };
+
 }
 
-const Gpio = require('onoff').Gpio;
-const ledPinNumbers = [9, 10, 11, 12];
-const pushButtonPins = [5, 6, 7, 8, ];
-const modeButton = new Gpio(4, 'in', 'rising', { debounceTimeout: 75 })
-const LED = ledPinNumbers.map((pin) => new Gpio(pin, 'out'));
-const pushButtons = pushButtonPins.map((pin) => new Gpio(pin, 'in', 'rising', { debounceTimeout: 75 }));
-const [LED1, LED2, LED3, LED4] = LED;
-const [pushButton1, pushButton2, pushButton3, pushButton4] = pushButtons;
-const buttons = [pushButton1, pushButton2, pushButton3, pushButton4];
-const leds = [LED1, LED2, LED3, LED4];
+// Function to stop button listeners
+function stopButtonListeners() {
+  buttons.forEach((button) => button.unwatchAll());
+}
+
+// Set mode to "mutesA" when the program starts
+let mode: string = "mutesA";
+console.log(mode)
+const initialIndexes = ledIndexMap[mode];
+updateSubscriptions();
+
+// Watch buttons
 buttons.forEach((button, index) => button.watch(handleMuteEvent(index + 1)));
-//leds.forEach((led, index) => subscribeAndControlLED(index + 1, led));
-leds.forEach((led, index) => subscribeLED(index + 1, led))
 
+// Initialize mode button and set up mode change logic
+const modeButton = new Gpio(4, 'in', 'rising', { debounceTimeout: 75 });
 modeButton.watch((err: any, value: any) => {
   if (err) {
     throw err;
   }
   if (value === 1) {
-  stopButtonListeners()
-  if (mode === "mutesA"){ mode = "mutesB" } else if (mode === "mutesB"){ mode = "sampler" } else if (mode === "sampler"){mode = "player" } else if (mode === "player"){ mode = "mutesA"}
-  console.log('Mode is now ', mode)
-  if (mode === "mutesA") {
-  buttons.forEach((button, index) => button.watch(handleMuteEvent(index + 1)));
-  leds.forEach((led, index) => subscribeAndControlLED(index + 1, led));
-} else if (mode === "mutesB") {
-  buttons.forEach((button, index) => button.watch(handleMuteEvent(index + 5)));
-  leds.forEach((led, index) => subscribeAndControlLED(index + 5, led));
-} else if (mode === "mutesB") {
-} else if (mode === "sampler") {
-  buttons.forEach((button, index) => button.watch(handleSamplerEvent(index + 1)));
-} else if (mode === "player"){}
-
+    stopButtonListeners();
+    modeIndex = (modeIndex + 1) % modes.length;
+    mode = modes[modeIndex]; // Update the mode variable
+    console.log('Mode now', mode);
+    updateSubscriptions();
+// Handle button events based on the mode
+  if (mode === "sampler") {
+    buttons.forEach((button, index) => button.watch(handleSamplerEvent(index + 1)));
+  } else if (mode === "player") {
+    buttons.forEach((button, index) => button.watch(handlePlayerEvent(index + 1)));
+  } else {
+    buttons.forEach((button, index) => button.watch(handleMuteEvent(index + 1)));
+  }
 }
 });
 
+// Function to update LED subscriptions based on the current mode
+function updateSubscriptions() {
+  unsubscribeLEDs(); // Unsubscribe from previous subscriptions
+  const indexes = ledIndexMap[mode];
+  if (indexes) {
+    for (let i = 0; i < leds.length; i++) {
+      subscribeLED(indexes[i], leds[i]);
+    }
+  }
+}
+
+// Handle program termination and unexport GPIO pins
 function unexportOnClose() {
   leds.forEach((led) => {
     led.writeSync(0);
@@ -112,4 +158,5 @@ function unexportOnClose() {
   });
 }
 
-process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
+process.on('SIGINT', unexportOnClose); // Function to run when the user closes using Ctrl+C
+
