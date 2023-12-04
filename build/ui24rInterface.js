eba4ea68126043dad712988f74df57e6f390d5b2
jebbgrenham <jebbgrenham@gmail.com>
2023-12-04 01:44:49 +0000

    moved ui24r interface out of main

--- build/ui24rInterface.js
+++ build/ui24rInterface.js
@@ -1,35 +1,12 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.mainInterface = void 0;
 const soundcraft_ui_connection_1 = require("soundcraft-ui-connection");
 const rxjs_1 = require("rxjs");
 const operators_1 = require("rxjs/operators");
 const child_process_1 = require("child_process");
-const index_1 = require("./index");
-function useConnection() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const conn = yield (0, index_1.connectAndReturnConn)();
-        if (conn) {
-            mainInterface(conn);
-            console.log('Connection is established:', conn);
-        }
-        else {
-            console.log('No connection');
-            // i2c display the connection failed
-        }
-    });
-}
-// Call the function to use the connection
-useConnection();
 function mainInterface(conn) {
+    console.log('main iface has been called');
     // Constants
     const Gpio = require('onoff').Gpio;
     const readline = require('readline');
@@ -281,3 +258,4 @@ function mainInterface(conn) {
     }
     process.on('SIGINT', unexportOnClose);
 } //end of mainInterface
+exports.mainInterface = mainInterface;
---
+++ src/ui24rInterface.ts
@@ -0,0 +1,285 @@
+import { SoundcraftUI } from 'soundcraft-ui-connection';
+import { PlayerState, MtkState } from 'soundcraft-ui-connection';
+import { interval, Subscription, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { exec, ExecException } from 'child_process';
+
+export function mainInterface(conn: SoundcraftUI) {
+  console.log('main iface has been called')
+  // Constants
+  const Gpio = require('onoff').Gpio;
+  const readline = require('readline');
+  const LED_OFF = 0;
+  const LED_ON = 1;
+  const DEBOUNCE_TIMEOUT = 75;
+  const ledPinNumbers = [9, 10, 11, 12];
+  const pushButtonPins = [5, 6, 7, 8];
+
+  // Define modes
+  const modes = ["mutesA", "mutesB", "player", "sampler"];
+  let modeIndex = 3; //so that on initial handleMode we get mutes A
+  let mode: string = "sampler"; // You will regret changing this...
+  console.log(mode);
+
+  // Define LED and button pins
+  const LED = ledPinNumbers.map((pin) => new Gpio(pin, 'out'));
+  const pushButtons = pushButtonPins.map((pin) => new Gpio(pin, 'in', 'rising', { debounceTimeout: DEBOUNCE_TIMEOUT }));
+
+  // LED and button arrays
+  const [LED1, LED2, LED3, LED4] = LED;
+  const [pushButton1, pushButton2, pushButton3, pushButton4] = pushButtons;
+  const buttons = [pushButton1, pushButton2, pushButton3, pushButton4];
+  const leds = [LED1, LED2, LED3, LED4];
+
+  // Define the LED index mapping
+  const ledIndexMap: { [mode: string]: (number | string)[] } = {
+    mutesA: ['all', 'fx', 1, 2],
+    mutesB: [3, 4, 5, 6],
+  };
+
+  // Create a map to track subscriptions
+  const subscriptionMap: { [index: number | string]: Subscription } = {};
+
+  // Initial LED subscription and mode setup
+  handleModeChange();
+
+  function subscribeLED(LEDindex: number | string, LED: { writeSync: (state: number) => void, readSync: () => number }) {
+    let index: number | string = LEDindex;
+    if (subscriptionMap[index]) {
+      subscriptionMap[index].unsubscribe(); // Unsubscribe previous subscription
+    }
+    if (mode === "player") {
+      if (index === 1) {
+        subscriptionMap[index] = conn.player.state$.subscribe((state: PlayerState) => {
+          if (state === PlayerState.Playing) {
+            LED.writeSync(LED_ON);
+          } else {
+            LED.writeSync(LED_OFF);
+          }
+        });
+      } else if (index === 4) {
+        // Led4 is based on recorder state
+        subscriptionMap[index] = conn.recorderMultiTrack.recording$.subscribe((recording: number) => {
+          if (recording == 1) {
+            LED.writeSync(LED_ON);
+          } else {
+            LED.writeSync(LED_OFF);
+          }
+        });
+      }
+    } else if (mode === "mutesA" || mode === "mutesB") {
+      subscriptionMap[index] = conn.muteGroup(index as any).state$.subscribe((state) => {
+        LED.writeSync(state);
+        console.log(`Read index: ${LEDindex} and set to:`, LED.readSync());
+      });
+    } else {
+      // Retain the original behavior for other modes
+    }
+  }
+
+  function unsubscribeLEDs() {
+    for (const led of leds) {
+      led.writeSync(LED_OFF); // Turn off the LED
+    }
+
+    for (const index in subscriptionMap) {
+      if (subscriptionMap.hasOwnProperty(index)) {
+        subscriptionMap[index].unsubscribe();
+        delete subscriptionMap[index];
+      }
+    }
+  }
+
+  function handleMuteEvent(buttonNumber: number) {
+    return (err: string, value: string) => {
+      if (!err) {
+        const group = ledIndexMap[mode][buttonNumber - 1];
+        console.log(mode);
+        console.log(group);
+        if (typeof group === 'number' || typeof group === 'string') {
+          conn.muteGroup(group as any).toggle();
+        }
+        console.log('Pushed Button:', group);
+      }
+    };
+  }
+
+
+
+  function handleSamplerEvent(buttonNumber: number) {
+    let audio: any = null;
+
+    return (err: ExecException | null, value: string | null) => {
+      if (!err) {
+        // Turn on the LED
+        leds[buttonNumber - 1].writeSync(LED_ON);
+        if (audio) {
+          audio.kill(); // Stop audio playback if the button is pressed again
+          leds[buttonNumber - 1].writeSync(LED_OFF);
+          audio = null;
+        } else {
+          console.log('trying to play');
+          const soundCommand = `pw-play /home/admin/samples/${buttonNumber}.wav`;
+
+          audio = exec(soundCommand, (err, stdout, stderr) => {
+            if (err) {
+              console.log(`Could not play sound/sound stopped: ${err}`);
+            } else {
+              console.log('Played sample', buttonNumber);
+              audio = null;
+            }
+            leds[buttonNumber - 1].writeSync(LED_OFF);
+          });
+        }
+      }
+    };
+  }
+
+  function handlePlayerEvent(buttonNumber: number) {
+    return (err: string, value: string) => {
+      if (!err) {
+        switch (buttonNumber) {
+          case 1:
+            handlePlayerButton1();
+            break;
+          case 2:
+            conn.player.prev();
+            break;
+          case 3:
+            conn.player.next();
+            break;
+          case 4:
+            conn.recorderMultiTrack.recordToggle();
+            break;
+        }
+      }
+    };
+  }
+
+  function handlePlayerButton1() {
+    let destroy$ = new Subject<void>();
+
+    conn.player.state$
+      .pipe(takeUntil(destroy$))
+      .subscribe((state: PlayerState) => {
+        if (state == PlayerState.Playing) {
+          console.log('stopping');
+          destroy$.next(); // Signal unsubscription
+          destroy$.complete();
+          conn.master.player(1).fadeTo(0, 3000);
+          setTimeout(() => {
+            conn.player.pause();
+          }, 3000);
+        } else {
+          console.log('playing');
+          conn.player.play();
+          conn.master.player(1).fadeToDB(-25, 3000);
+          destroy$.next(); // Signal unsubscription
+          destroy$.complete();
+        }
+      });
+  }
+
+  function stopButtonListeners() {
+    buttons.forEach((button) => button.unwatchAll());
+  }
+
+  function handleModeChange() {
+    stopButtonListeners();
+    modeIndex = (modeIndex + 1) % modes.length;
+    mode = modes[modeIndex];
+    console.log('Mode now', mode);
+    updateSubscriptions();
+
+    if (mode === "sampler") {
+      buttons.forEach((button, index) => button.watch(handleSamplerEvent(index + 1)));
+    } else if (mode === "player") {
+      buttons.forEach((button, index) => button.watch(handlePlayerEvent(index + 1)));
+    } else {
+      buttons.forEach((button, index) =>
+      button.watch(handleMuteEvent(index + 1)));
+    }
+  }
+
+  // Initialize mode button and set up mode change logic
+  const modeButton = new Gpio(4, 'in', 'both', { debounceTimeout: DEBOUNCE_TIMEOUT });
+
+  let isModeButtonPressed = false;
+  let shutdownTimeout: NodeJS.Timeout | null = null;
+
+  function handleShutdown() {
+    console.log('Shutting down...');
+    executeShutdownCommand();
+  }
+
+  modeButton.watch((err: any, value: any) => {
+    if (err) {
+      throw err;
+    }
+
+    if (value === 0) {
+      isModeButtonPressed = true;
+      shutdownTimeout = setTimeout(() => {
+        if (isModeButtonPressed) {
+          handleShutdown();
+        }
+        shutdownTimeout = null;
+      }, 3000);
+    } else if (value === 1) {
+      if (isModeButtonPressed) {
+        handleModeChange();
+      }
+      isModeButtonPressed = false;
+
+      if (shutdownTimeout) {
+        clearTimeout(shutdownTimeout);
+        shutdownTimeout = null;
+      }
+    }
+  });
+
+  function updateSubscriptions() {
+    unsubscribeLEDs();
+    const indexes = ledIndexMap[mode];
+    if (indexes) {
+      for (let i = 0; i < leds.length; i++) {
+        subscribeLED(indexes[i], leds[i]);
+      }
+    }
+
+    if (mode === "player") {
+      subscribeLED(1, LED1);
+      subscribeLED(4, LED4);
+    }
+  }
+
+  readline.emitKeypressEvents(process.stdin);
+  if (process.stdin.isTTY) process.stdin.setRawMode(true);
+
+  function unexportOnClose() {
+    leds.forEach((led) => {
+      led.writeSync(LED_OFF);
+      led.unexport();
+    });
+
+    pushButtons.forEach((button) => {
+      button.unexport();
+    });
+  }
+
+  function executeShutdownCommand() {
+    exec('sudo shutdown -h now', (error: Error | null, stdout: string, stderr: string) => {
+      if (error) {
+        console.error(`Error during shutdown: ${error}`);
+      } else {
+        console.log('Shutdown initiated successfully.');
+      }
+    });
+  }
+
+  process.on('SIGINT', unexportOnClose);
+
+
+} //end of mainInterface
+
+
